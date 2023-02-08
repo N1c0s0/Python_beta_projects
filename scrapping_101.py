68b22cb5b9e8f3a0e84ac0172f9738518439a6b9
N1c0s0 <aijn1399@gmail.com>
2023-02-07 23:56:21 -0500

    every item on page is scrapped

--- scrapping_101.py
+++ scrapping_101.py
@@ -3,15 +3,15 @@ import requests
 
 html_text = requests.get('https://listado.mercadolibre.com.co/microfono#D[A:microfono]').text
 soup = BeautifulSoup(html_text, 'lxml')
-products = soup.find("li", class_= "ui-search-layout__item shops__layout-item")
-#print(products.prettify())
-name = products.find("h2", class_ = "ui-search-item__title shops__item-title").text
+products = soup.find_all("li", class_= "ui-search-layout__item shops__layout-item")
+for product in products:
+    #print(products.prettify())
+    name = product.find("h2", class_ = "ui-search-item__title shops__item-title").text
 
-price = products.find("span", class_ ="price-tag-text-sr-only").text
+    price = product.find("span", class_ ="price-tag-text-sr-only").text
 
 
-
-print(price)
-
-print(name)
-
+    print("----------------")
+    print(price)
+    print(name)
+    print("----------------")
